09877f08710ddfc7d8045096272481b1b65d01ab
Daniel Larkin <realdanieljlarkin@gmail.com>
2023-04-07 13:30:59 -0500

    Added analytics

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useRef } from 'react'
-
 import { Footer } from '@/components/Footer'
 import { Header } from '@/components/Header'
+import { Analytics } from '@vercel/analytics/react'
 
 import '@/styles/tailwind.css'
 import 'focus-visible'
@@ -51,6 +51,7 @@ export default function App({ Component, pageProps, router }) {
         </main>
         <Footer />
       </div>
+      <Analytics />
     </>
   )
 }
